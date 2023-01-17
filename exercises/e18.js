a537813ddf58c2b814fb9cd491cd5f162b086fd4
jjhiggz <jonathan.higger@gmail.com>
2023-01-17 15:05:00 -0700

    fixed joinToString

--- exercises/e18.js
+++ exercises/e18.js
@@ -1,16 +1,10 @@
-
 // EXERCISE 18
 // Please, read the exercise-info/join.md to get the initial data of what is the expected result of this exercise.
 // Array example: joinToStringData in /data/data.js
 // joinToString(['a', 'b', 'c'], '-') => 'a-b-c'
 // NOTE: You can NOT use the array.join(), array.toString(), and array.replace() methods in your code
 
-export function joinToString(array, separator) {
-  // Your code goes here...
-
-}
-
-
+export function joinToString(array, separator) {}
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-18"
--- test/e18.test.js
+++ test/e18.test.js
@@ -1,12 +1,29 @@
 import { describe, expect, it } from "vitest";
 import { joinToString } from "../exercises/e18";
-import { joinToStringData } from "../data/data";
 
-describe('isArrayIncludes', () => {
-  it('Should exist', () => {
+describe("isArrayIncludes", () => {
+  it("Should exist", () => {
     expect(joinToString).toBeInstanceOf(Function);
   });
-  it('Should return a correct string with symbols between array items values only', () => {
-    expect(joinToString(joinToStringData, ' ')).toEqual('Devslopes is teaching me FOR loops and functions!');
+  it("Should return a correct string with symbols between array items values only", () => {
+    expect(
+      joinToString(
+        [
+          "Devslopes",
+          "is",
+          "teaching",
+          "me",
+          "FOR",
+          "loops",
+          "and",
+          "functions",
+          "!",
+        ],
+        " "
+      )
+    ).toEqual("Devslopes is teaching me FOR loops and functions !");
+  });
+  it("should combine the first few letters of the alpha bet", () => {
+    expect(joinToString(["a", "b", "c", "d"], "-")).toBe("a-b-c-d");
   });
 });
