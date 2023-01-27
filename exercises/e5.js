da9941c1e42512fb3073bbe76ff982ea932e9321
Steven Brcak <sbrcak@live.com>
2023-01-26 19:32:55 -0800

    Completed e5.js

--- exercises/e5.js
+++ exercises/e5.js
@@ -5,8 +5,11 @@
 // getAllClientNames(bankAccounts) => ['Kevin', 'Spenser']
 
 export function getAllClientNames(array) {
-  // Your code goes here...
-
+  let newArray = [];
+  for(let i = 0; i < array.length; i++){
+    newArray.push(array[i].name);
+  }
+  return newArray;
 }
 
 
