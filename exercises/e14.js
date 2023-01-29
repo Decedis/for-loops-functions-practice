b2ebf8a39e0d861d2bbd52b52e939c62dd0072f7
Steven Brcak <sbrcak@live.com>
2023-01-28 22:10:08 -0800

    Completed e14.js

--- exercises/e14.js
+++ exercises/e14.js
@@ -6,8 +6,26 @@
 // getClientsWithWrongBalance(bankAccounts) => [{ name: 'Name1', balance: 32, ... }, { name: 'Name2', balance: 3523, ... }]
 
 export function getClientsWithWrongBalance(array) {
-  // Your code goes here...
+  let wrongBalance = [];
+  let depositSum = 0;
+  let withdrawalSum = 0;
 
+  for (let i = 0; i < array.length; i++) {
+    if ((array[i].deposits != null) && (array[i].withdrawals != null)) {
+      for (let j = 0; j < array[i].deposits.length; j++) {
+        depositSum += array[i].deposits[j];
+      }
+      for (let j = 0; j < array[i].withdrawals.length; j++) {
+        withdrawalSum += array[i].withdrawals[j];
+      }
+      if ((depositSum - withdrawalSum) != array[i].balance) {
+        wrongBalance.push(array[i]);
+      }
+      depositSum = 0;
+      withdrawalSum = 0;
+    }
+  }
+  return wrongBalance;
 }
 
 
