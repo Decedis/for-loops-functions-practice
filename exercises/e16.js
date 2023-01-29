e5d66c656740829d37c0dee984bd998111c4da00
Steven Brcak <sbrcak@live.com>
2023-01-28 22:28:39 -0800

    Complete e16.js

--- exercises/e16.js
+++ exercises/e16.js
@@ -6,8 +6,18 @@
 // NOTE: You can NOT use the array.flat() method in your code
 
 export function flatArrays(array) {
-  // Your code goes here...
+  let flatArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].length === undefined) {
+      flatArray.push(array[i])
+    } else {
+      for (let j = 0; j < array[i].length; j++) {
+        flatArray.push(array[i][j]);
+      }
+    }
 
+  }
+  return flatArray;
 }
 
 
