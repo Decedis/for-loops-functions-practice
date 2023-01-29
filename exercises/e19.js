79024925b62324d3f8a0412326159ca76e27f554
Steven Brcak <sbrcak@live.com>
2023-01-29 00:33:25 -0800

    Complete e19.js. This was one of the easier ones.

--- exercises/e19.js
+++ exercises/e19.js
@@ -5,8 +5,11 @@
 // NOTE: You can NOT use the array.reverse() method in your code
 
 export function reverseArray(array) {
-  // Your code goes here...
-
+  let newArray = [];
+  for(let i = array.length - 1; i >= 0; i--){
+    newArray.push(array[i]);
+  }
+  return newArray;
 }
 
 
